0d00600198086ee61f29e64550f70fb87179753f
Hojung <106735512+ristukaJJang@users.noreply.github.com>
2025-10-01 21:48:39 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.20 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = Integer.MIN_VALUE;
+
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = 0; j < numbers.length; j++) {
+                if(numbers[i] * numbers[j] > answer && i != j) 
+                    answer = numbers[i] * numbers[j];
+                else continue;
+            }
+        }
+        return answer;
+    }
+}
