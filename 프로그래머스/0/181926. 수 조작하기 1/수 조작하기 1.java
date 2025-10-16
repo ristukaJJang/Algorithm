e22d20df3521b110e9063d0ffb4044ca61594c02
Hojung <106735512+ristukaJJang@users.noreply.github.com>
2025-10-16 21:28:17 +0900

    [level 0] Title: 수 조작하기 1, Time: 11.01 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = 0;
+        for(int i = 0; i < control.length(); i++) {
+            if(control.charAt(i) == 'w') n++;
+            else if(control.charAt(i) == 's') n--;
+            else if(control.charAt(i) == 'd') n += 10;
+            else n -= 10;
+        }
+        answer = n;
+        return answer;
+    }
+}
