4cb1a4472774047b1544ab0d8459706915e9c07e
Hojung <106735512+ristukaJJang@users.noreply.github.com>
2025-10-17 12:32:57 +0900

    [level 0] Title: 꼬리 문자열, Time: 2.42 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String answer = "";
+        for(int i = 0; i < str_list.length; i++) {
+            if(str_list[i].contains(ex)) {
+                str_list[i] = "";
+            }
+        }
+        
+        for(int i = 0; i < str_list.length; i++) {
+            answer += str_list[i];
+        }
+        
+        return answer;
+    }
+}
