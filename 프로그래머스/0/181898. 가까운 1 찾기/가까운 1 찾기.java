ee8839c214dd0a6fceb251f3c7e74084bb4dbfd6
Hojung <106735512+ristukaJJang@users.noreply.github.com>
2025-10-20 14:11:35 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.41 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = -1;
+        for(int i = 0; i < arr.length; i++) {
+            if(i >= idx && arr[i] == 1) return i;
+        }
+        return answer;
+    }
+}
