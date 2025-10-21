bb7f22224abdf4443654a93e8a9419831350ce07
Hojung <106735512+ristukaJJang@users.noreply.github.com>
2025-10-21 15:03:20 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int sum = 0;
+        for(int i = 0; i < signs.length; i++) {
+            if(signs[i] == true) sum += absolutes[i];
+            else sum -= absolutes[i];
+        }
+        return sum;
+    }
+}
