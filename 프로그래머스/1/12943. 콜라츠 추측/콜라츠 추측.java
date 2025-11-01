a7997e255f300cf35b8741def01d7975284a19f5
Hojung <106735512+ristukaJJang@users.noreply.github.com>
2025-11-01 21:54:40 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = num;
+        if(n == 1) return 0;
+        
+        while(n != 1) {
+            if(n % 2 == 0) n /= 2;
+            else if(n % 2 != 0) n = n * 3 + 1;
+            answer++;
+            
+        }
+        if(answer >= 500) answer = -1;
+        return answer;
+    }
+}
